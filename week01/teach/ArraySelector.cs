50a6e43eff9d3890c00d3d9745d2b394b90506ec
Lee <karasactualemail@gmail.com>
2026-08-31 10:31:38 -0600

    w1 learning

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,22 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        var l1 = 0;
+        var l2 = 0;
+
+        for (var i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[l1++];
+            } else
+            {
+                result[i] = list2[l2++];
+            }
+        }
+
+        return result;
+        
     }
 }
