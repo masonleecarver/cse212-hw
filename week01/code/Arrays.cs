d26502556a34def571068449d8dc13538d24ee75
Lee <karasactualemail@gmail.com>
2026-09-01 12:13:44 -0600

    arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 public static class Arrays
 {
     /// <summary>
@@ -8,12 +11,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // create the var for the resulting array
+        
+        var result = new double[length];
 
-        return []; // replace this return statement with your own
+        // populate the array with the multiples of the supplied number
+
+        for (var i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        // return the resulting array
+
+        return result; 
     }
 
     /// <summary>
@@ -25,9 +36,29 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //make result to add the rotated list to
+        var result = new List<int>();
+        // set variable to get the index of data you need
+        var wrap = data.Count - amount;
+        for (var i = 0; i < data.Count; i++)
+        {
+            // this resets the index so once it's out of range the next thing copied will be the first. 
+            if (wrap >= data.Count)
+            {
+                wrap = 0;
+            }
+            
+            result.Add(data[wrap]);
+
+            Debug.WriteLine(string.Join(", ", result));
+
+            // incriment wrap to move to the next item in data
+            wrap++;
+        }
+
+        // replace data with the result
+        data.Clear();
+        data.AddRange(result);
     }
+
 }
